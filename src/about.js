aaa1dda1c0ca5a2f91ea0800572782d416379905
BenerdAndrian <ben527466@gmail.com>
2025-02-18 06:36:22 +0000

    .

--- src/about.js
+++ src/about.js
@@ -1,9 +1,17 @@
+import telphone from "../telphone.jpg"
+
 export function introductory(){
     const theContent=document.getElementById("content")
     theContent.innerHTML=""
     const h2=document.createElement("h2")
     const h3=document.createElement("h3")
+    const img=document.createElement("img");
+    const div=document.createElement("div")
     
+    img.src=`${telphone}`;
+    const theDivContainer=document.createElement("div")
+    
+    theDivContainer.setAttribute("class","flex")
     h2.textContent="Contact"
     h2.setAttribute("class","centerText")
     theContent.appendChild(h2)
@@ -12,8 +20,9 @@ export function introductory(){
         h3.textContent=part;
         const p1=document.createElement("p")
         const p2=document.createElement("p")
-        const div=document.createElement("div")
-        div.setAttribute("class","homeBoard")
+      
+        const p=document.createElement("p")
+        p.setAttribute('class','para')
         if(part==="Contacter"){
            
                p1.innerHTML=` 
@@ -24,8 +33,10 @@ export function introductory(){
                                <span class="strong">Phone Number: </span>
                                0976445875
                `
-               div.appendChild(p1);
-               div.appendChild(p2);
+               p.appendChild(p1);
+               p.appendChild(p2)
+               div.appendChild(p);
+             
         }else if(part==="Manager"){
             p1.innerHTML=` 
             <span class="strong">Email: </span>
@@ -35,10 +46,12 @@ export function introductory(){
                         <span class="strong">Phone Number: </span>
                         0976442235
             `
-            div.appendChild(p1)
-            div.appendChild(p2)
+            p.appendChild(p1)
+            p.appendChild(p2)
+            div.appendChild(p);
         }
-        theContent.appendChild(div);
+        theDivContainer.appendChild(div)
+        theContent.appendChild(theDivContainer);
     }
-   
+    theDivContainer.appendChild(img)
 }
